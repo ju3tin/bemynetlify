0d0e81f4893118adb6271e51b613efe21b509717
MooseMan <ssaciolo@gmail.com>
2020-02-03 17:52:02 -0500

    Fixing Conflict

--- Anywhere-Fitness/src/reducers/fitnessReducer.js
+++ Anywhere-Fitness/src/reducers/fitnessReducer.js
@@ -1,23 +1,29 @@
 const initialState = {
-    isLoading: false,
-    users: [],
-    error: false
-}
+    users: {
+        username: "",
+        email: "",
+        password: "",
+        role: ""
+    }
+//   isLoading: false,
+//   users: [],
+//   error: false
+// }
 
 export const fitnessReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case LOGIN_START:
-            return {
-                ...state,
-                isLoading: true
-            }
-        case LOGIN_SUCCESS:
-            return {
-                ...state,
-                isLoading: false,
-                users: action.payload
-            } 
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        ...state,
+        isLoading: true
+      }
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        users: action.payload
+      }
+    default:
+      return state
+  }
 }
