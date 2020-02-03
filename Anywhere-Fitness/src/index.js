53cadbedd6d95e9c4cdeeadb83d2691eee07844b
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2020-02-03 16:53:59 -0300

    set up reducer and Redux store

--- Anywhere-Fitness/src/index.js
+++ Anywhere-Fitness/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { reducer, fitnessReducer } from "./reducers/fitnessReducer";
+
+const store = createStore(fitnessReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
