00b6a034b54f78b0842b769754aec1dda6327f69
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2020-02-05 11:50:04 -0300

    latest edits

--- Anywhere-Fitness/src/reducers/fitnessReducer.js
+++ Anywhere-Fitness/src/reducers/fitnessReducer.js
@@ -1,4 +1,11 @@
-import { GET_CLASSES_START, GET_CLASSES_SUCCESS, GET_USER_START, GET_USER_SUCCESS } from "../actions";
+import { 
+  GET_CLASSES_START, 
+  GET_CLASSES_SUCCESS, 
+  GET_USER_START, 
+  GET_USER_SUCCESS,
+  EDIT_USER_START,
+  EDIT_USER_SUCCESS
+} from "../actions";
 
 const initialState = {
   user: {
@@ -37,6 +44,17 @@ export const fitnessReducer = (state = initialState, action) => {
         ...state,
         isLoading: false,
         classes: action.payload
+      }
+    case EDIT_USER_START:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case EDIT_USER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        user: action.payload
       }
     default:
       return state
