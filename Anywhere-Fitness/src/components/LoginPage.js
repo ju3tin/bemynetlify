433c3f13968b6a9570e85e888f56f091ed912786
MooseMan <ssaciolo@gmail.com>
2020-02-06 14:18:44 -0500

    Fixed more conflicts

--- Anywhere-Fitness/src/components/LoginPage.js
+++ Anywhere-Fitness/src/components/LoginPage.js
@@ -1,21 +1,20 @@
-
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { connect } from "react-redux";
-import { loginAndGetUser } from "../actions";
+import React, { useState } from "react"
+import { useForm } from "react-hook-form"
+import { connect } from "react-redux"
+import { loginAndGetUser } from "../actions"
 import RegisterPage from "../components/RegisterPage"
-import styled from 'styled-components'
+import styled from "styled-components"
 
 const StyledH1 = styled.h1`
-color: white;
-text-shadow: -1px -1px 0 #FF5500, 1px -1px 0 #FF5500, -1px 1px 0 #FF5500, 1px 1px 0 #FF5500;
-//color: black;
-//color: rgb(229, 294, 255)
-  @media (max-width: 500px){
+  color: white;
+  text-shadow: -1px -1px 0 #ff5500, 1px -1px 0 #ff5500, -1px 1px 0 #ff5500,
+    1px 1px 0 #ff5500;
+  //color: black;
+  //color: rgb(229, 294, 255)
+  @media (max-width: 500px) {
     margin: 5% 0 0 0%;
   }
 `
-
 
 const LoginPage = props => {
   const [login, setLogin] = useState({ username: "", password: "" })
@@ -36,45 +35,44 @@ const LoginPage = props => {
   }
 
   return (
+    <>
+      <form className="App" onSubmit={handleSubmit(onSubmit)}>
+        <StyledH1>Log In</StyledH1>
+        <hr></hr>
 
-    <form className="App" onSubmit={handleSubmit(onSubmit)}>
-      <StyledH1>Log In</StyledH1>
-      <hr></hr>
+        <label htmlFor="username">Username:</label>
+        <input
+          onChange={changeHandler}
+          placeholder="Username..."
+          type="text"
+          name="username"
+          ref={register({ required: true, minLength: 3 })}
+        />
+        {errors.username && errors.username.type === "required" && (
+          <p className="starterPs">This field is required.</p>
+        )}
+        {errors.username && errors.username.type === "minLength" && (
+          <p className="starterPs">
+            This field requires a minimum length of 3 characters.
+          </p>
+        )}
 
-      <label htmlFor="username">Username:</label>
-      <input
-        onChange={changeHandler}
-        placeholder="Username..."
-        type="text"
-        name="username"
-        ref={register({ required: true, minLength: 3 })}
-      />
-      {errors.username && errors.username.type === "required" && (
-        <p className="starterPs">This field is required.</p>
-      )}
-      {errors.username && errors.username.type === "minLength" && (
-        <p className="starterPs">
-          This field requires a minimum length of 3 characters.
-        </p>
-      )}
-
-      <label htmlFor="password">Password:</label>
-      <input
-        onChange={changeHandler}
-        placeholder="Password..."
-        name="password"
-        type="password"
-        ref={register({ required: true, minLength: 5 })}
-      />
-      {errors.password && errors.password.type === "required" && (
-        <p className="starterPs">This field is required.</p>
-      )}
-      {errors.password && errors.password.type === "minLength" && (
-        <p className="starterPs">
-          This field requires a minimum length of 5 characters.
-        </p>
-      )}
-
+        <label htmlFor="password">Password:</label>
+        <input
+          onChange={changeHandler}
+          placeholder="Password..."
+          name="password"
+          type="password"
+          ref={register({ required: true, minLength: 5 })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <p className="starterPs">This field is required.</p>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <p className="starterPs">
+            This field requires a minimum length of 5 characters.
+          </p>
+        )}
 
         <input type="submit" className="submitButton2" />
       </form>
